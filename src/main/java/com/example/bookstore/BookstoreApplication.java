f3e987c6166441e7b9ca73037e89ec44e41b4a32
Jami Äijänen <jami.aijanen@hotmail.com>
2021-10-06 19:45:07 +0300

    kirjautuminen

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
+import com.example.bookstore.domain.User;
+import com.example.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -21,7 +23,8 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository Crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository Crepository,
+			UserRepository urepository) {
 		return (args) -> {
 
 			Crepository.save(new Category("Fantasy"));
@@ -32,6 +35,11 @@ public class BookstoreApplication {
 					Crepository.findByName("Fantasy").get(0)));
 			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", "1945",
 					Crepository.findByName("Horror").get(0)));
+
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 
 		};
 	}
