923ea0f4f3d2881b14c3f5bd4246c9e53847df54
Jami Äijänen <jami.aijanen@hotmail.com>
2021-10-14 16:38:10 +0300

    update

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -2,17 +2,8 @@ package com.example.bookstore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.bookstore.domain.Book;
-import com.example.bookstore.domain.BookRepository;
-import com.example.bookstore.domain.Category;
-import com.example.bookstore.domain.CategoryRepository;
-import com.example.bookstore.domain.User;
-import com.example.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -22,25 +13,25 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository Crepository,
-			UserRepository urepository) {
-		return (args) -> {
-
-			Crepository.save(new Category("Fantasy"));
-			Crepository.save(new Category("Comic"));
-			Crepository.save(new Category("Horror"));
-
-			repository.save(new Book("Ernest Hemingway", "A Farewell to Arms", "1232323-21", "1929",
-					Crepository.findByName("Fantasy").get(0)));
-			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", "1945",
-					Crepository.findByName("Horror").get(0)));
-
-			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
-			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-			urepository.save(user1);
-			urepository.save(user2);
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository Crepository,
+//			UserRepository urepository) {
+//		return (args) -> {
+//
+//			Crepository.save(new Category("Fantasy"));
+//			Crepository.save(new Category("Comic"));
+//			Crepository.save(new Category("Horror"));
+//
+//			repository.save(new Book("Ernest Hemingway", "A Farewell to Arms", "1232323-21", "1929",
+//					Crepository.findByName("Fantasy").get(0)));
+//			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", "1945",
+//					Crepository.findByName("Horror").get(0)));
+//
+//			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+//			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+//			urepository.save(user1);
+//			urepository.save(user2);
+//
+//		};
+//	}
 }
